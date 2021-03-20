9e18d7d684b6beb11719044ae660a309f17f1ef2
Han Gao <hanga@google.com>
2021-03-20 00:29:34 +0000

    container-build pipeline

---
+++ test_manager/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"guest-test-infra/test_manager/test_manager"
+)
+
+
+func main() {
+	fmt.Printf("hello world")
+	test_manager.DoNothing()
+}
---
+++ test_manager/test_manager/test_manager.go
@@ -0,0 +1,6 @@
+package test_manager
+
+func DoNothing(){
+
+}
+
